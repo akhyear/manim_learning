708b4cc5cdb78d2fbc3381fbc3c8e201ff237175
akhyear <rafinislam66@gmail.com>
2024-10-15 06:23:03 +0600

    Adding function.py file

--- functioin.py
+++ functioin.py
@@ -1,1 +1,28 @@
-  
+# def five_times(x):
+#     return 5*x
+
+# a = five_times(9)
+
+# def add_num(first,second):
+#     sum = first+second
+#     print(sum)
+
+# add_num(56,10)
+# add_num(334,1)
+
+# def add(x,y):
+#     return x+y 
+# sum = add(45,98)
+# # print(sum)
+
+def is_odd(x):
+    if x%2==0:
+        return False
+    else:
+        return True
+a = is_odd(99)
+print(a)
+
+def greet(name):
+    print(f"hello {name}")
+greet("Rafin")
