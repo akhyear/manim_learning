cc6f583c861a74866783ceacea8edce4281e698c
akhyear <rafinislam66@gmail.com>
2024-10-15 20:21:28 +0600

    Adding square_into_circle.py and with this video file

--- scene.py
+++ scene.py
@@ -7,10 +7,10 @@ class CreateCircle(Scene):
         circle.set_fill(PINK, opacity=0.5)  # set the color and transparency
         self.play(Create(circle))  # show the circle on screen'''
 
-from manim import *
+# from manim import *
 
-class CreateCircle(Scene):
-    def construct(self):
-        circle= Circle(radius=2,color=GREEN)
-        circle.set_fill(color=RED, opacity=1)
-        self.play(Create(circle))
+# class CreateCircle(Scene):
+#     def construct(self):
+#         circle= Circle(radius=2,color=GREEN)
+#         circle.set_fill(color=RED, opacity=1)
+#         self.play(Create(circle))
