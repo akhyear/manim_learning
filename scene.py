256280fc9a2059b8f8f1ebc8caea56b78ce39197
akhyear <rafinislam66@gmail.com>
2024-10-15 19:57:42 +0600

    Adding first file scene.py with other animation

---
+++ scene.py
@@ -0,0 +1,16 @@
+'''from manim import *
+
+
+class CreateCircle(Scene):
+    def construct(self):
+        circle = Circle()  # create a circle
+        circle.set_fill(PINK, opacity=0.5)  # set the color and transparency
+        self.play(Create(circle))  # show the circle on screen'''
+
+from manim import *
+
+class CreateCircle(Scene):
+    def construct(self):
+        circle= Circle(radius=2,color=GREEN)
+        circle.set_fill(color=RED, opacity=1)
+        self.play(Create(circle))
