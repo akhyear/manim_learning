881da49017fc3401af96ff375a22b4f320f0d401
akhyear <rafinislam66@gmail.com>
2024-10-21 10:26:45 +0600

    creating Tutorial 2 folder and move rest file (1-10) to this folder and adding 2.1.py file

---
+++ 2.1.py
@@ -0,0 +1,5 @@
+from manim import*
+
+class MovingObject(Scene):
+    class construct(self):
+        
---
+++ Tutorial 2/a.py
@@ -0,0 +1,14 @@
+from manim import *
+
+class SquareToCircle(Scene):
+    def construct(self):
+        green_square = Square(color=GREEN, fill_opacity=0.5)
+        self.play(DrawBorderThenFill(green_square))
+        blue_circle = Circle(color=BLUE, fill_opacity=0.5)
+        self.play(ReplacementTransform(green_square, blue_circle))
+        # এখানে Transform ব্যাবহার করা হয়নি কারন এটা ব্যবহার করলে blue_circle fade_out 
+        # করার সময় একটা ধাপ দেয় মানে green_square কেও কি কি বা করে।  তাই ReplacementTransform ব্যাবহার করতে হবে
+        # যাতে একটা আরেকটাকে রিপ্লেস করে
+        self.play(Indicate(blue_circle))
+        # এটা দিয়ে একটু যুম করে
+        self.play(FadeOut(blue_circle))
